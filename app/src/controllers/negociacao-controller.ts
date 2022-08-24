6044054cf18b93734336e3a8a721f5f69eca5d6f
Mariana Abreu <marianaabreuaffonso@gmail.com>
2022-08-24 20:50:49 -0300

    modulo5

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -7,6 +7,7 @@ import { logarTempoDeExecucao } from '../decorators/logar-tempo-de-execucao.js'
 import { inspect } from '../decorators/inspect.js';
 import { domInjector } from '../decorators/dom-injector.js';
 import { NegociacoesService } from '../services/negociacoes-service.js';
+import { imprimir } from '../utils/imprimir.js';
 
 export class NegociacaoController {
     @domInjector('#data')
@@ -43,6 +44,7 @@ export class NegociacaoController {
         }
 
         this.negociacoes.adiciona(negociacao);
+        imprimir(negociacao, this.negociacoes);
         this.limparFormulario();
         this.atualizaView();
     }
